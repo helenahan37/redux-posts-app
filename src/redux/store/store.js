e718b61b5b7a9a4ed0774f73b287fdf30a09334f
Helena.Han <hlyhelena37@gmail.com>
2024-01-02 21:16:38 +1100

    create frontend

---
+++ src/redux/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "../slice/postsSlice";
+
+//store
+const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+  },
+});
+
+export default store;
